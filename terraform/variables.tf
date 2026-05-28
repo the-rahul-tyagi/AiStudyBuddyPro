dfc2073c2dc397b0e1e0eefbe1f85aa8787aad49
the-rahul-tyagi <rahultyagiji6398@gmail.com>
2026-05-29 02:27:26 +0530

    Added Docker Prometheus config

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t3.micro"
+  default     = "t3.small"
 }
 
 variable "instance_name" {
